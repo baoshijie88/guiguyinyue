6a811bd036281ce0a4b6862aa99a0969d15b63c2
jinsir <123@qq.com>
2022-11-27 00:01:59 +0800

    11.26 排行榜添加动态数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,9 @@ Page({
         //轮播图列表
         bannerList: [],
         //推荐列表
-        recommendList: []
+        recommendList: [],
+        //排行榜列表
+        topList: [],
     },
     async onLoad() {
         let res = await request("/banner", {type: 2});
@@ -20,6 +22,16 @@ Page({
         this.setData({
             recommendList: rec.result
         })
+        let idx = 0;
+        let topArray = [];
+        while(idx < 5) {
+            let topRes = await request("/top/list", {idx: idx++});
+            //将原数据进行处理, 拼接成新的数据项
+            let topItem = {name: topRes.playlist.name, tracks: topRes.playlist.tracks.slice(0, 4)};
+            topArray.push(topItem);
+            this.setData({topList: topArray});
+        }
+
     },
     onMyEvent: function(e){
         console.log(e.detail) // 自定义组件触发事件时提供的 detail 对象
